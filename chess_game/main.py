3a5be141ea2320de17fc188f731cb5861b6a3c04
Eljowe <joelwickstroem@gmail.com>
2024-03-13 00:53:01 +0200

    table

--- chess_game/main.py
+++ chess_game/main.py
@@ -28,7 +28,7 @@ from LenovoAgent import LenovoAgent
 def main():
     ############### Set the players ###############
     #players = [Agent, MinimaxAgent]
-    players = [LenovoAgent, TestingAgent]
+    players = [Agent2, TestingAgent]
     #players = [Agent, DLAgent]
     #players = [Agent, RandomAgent]
     #players = [Agent, FishAgent]
